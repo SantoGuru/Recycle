e59eb97b9ac9baba7f4f61ec343328fb1692ac82
bruno-dv2 <brunoge3@edu.unifor.br>
2025-02-24 17:49:18 -0300

    adiciona entidades JPA e estrutura de pacotes

---
+++ backend/src/main/java/br/com/recycle/backend/model/Saida.java
@@ -0,0 +1,34 @@
+package br.com.recycle.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "saidas")
+public class Saida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "material_id", insertable = false, updatable = false)
+    private Long materialId;
+
+    @Column(nullable = false)
+    private Float quantidade;
+
+    @CreationTimestamp
+    private LocalDateTime data;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id")
+    private Material material;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id")
+    private Estoque estoque;
+}
