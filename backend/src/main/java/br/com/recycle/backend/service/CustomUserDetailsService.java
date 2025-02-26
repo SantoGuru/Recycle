99be035881f501c44d74c6bf7bf1638a27e6ba10
bruno-dv2 <brunoge3@edu.unifor.br>
2025-02-25 21:01:18 -0300

    implementa serviço para carregar usuários na autenticação

---
+++ backend/src/main/java/br/com/recycle/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package br.com.recycle.backend.service;
+
+import br.com.recycle.backend.model.Usuario;
+import br.com.recycle.backend.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + username));
+
+        return new User(usuario.getEmail(), usuario.getSenha(), new ArrayList<>());
+    }
+}
