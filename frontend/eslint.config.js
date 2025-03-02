38e35cb4cbe314b1e6371739194fb1e92fc30b8d
Walther Oliveira <waltherpires@hotmail.com>
2025-03-02 02:51:44 -0300

    chore(frontend): configura eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,38 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['dist'] },
+  pluginJs.configs.recommended, 
+  pluginReact.configs.flat.recommended, 
+  prettier,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      globals: globals.browser, 
+      ecmaVersion: "latest", 
+      sourceType: "module", 
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: "detect" }, 
+    },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: pluginReact, 
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "no-console": "warn", 
+      "react/prop-types": "off", 
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
   },
-]
+];
