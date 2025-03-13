f15ca8656a050a36b5f964b89220af0470bed841
Matheus Matos <mathewsmts2005@gmail.com>
2025-03-13 15:56:58 -0300

    Features

--- backend/src/main/java/br/com/recycle/backend/controller/EstoqueController.java
+++ backend/src/main/java/br/com/recycle/backend/controller/EstoqueController.java
@@ -2,12 +2,15 @@ package br.com.recycle.backend.controller;
 
 import br.com.recycle.backend.dto.EstoqueResponseDTO;
 import br.com.recycle.backend.service.EstoqueService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Estoque", description = "Endpoints para o gerenciamento de estoques")
 @RestController
 @RequestMapping("/api/estoques")
 public class EstoqueController {
@@ -18,6 +21,10 @@ public class EstoqueController {
     public EstoqueController(EstoqueService estoqueService) {
         this.estoqueService = estoqueService;
     }
+    @Operation(
+        summary = "Listar todos os Estoques",
+        description = "Retorna uma lista com todos os registros de Estoque disponiveis"
+    )
 
     @GetMapping
     public ResponseEntity<List<EstoqueResponseDTO>> listarTodos() {
@@ -26,6 +33,10 @@ public class EstoqueController {
 
         return ResponseEntity.ok(estoques);
     }
+    @Operation(
+        summary = "Buscar Estoque por ID",
+        description = "Retorna um registro de estoque especifico com base no ID informado"
+    )
 
     @GetMapping("/{id}")
     public ResponseEntity<EstoqueResponseDTO> buscarPorId(@PathVariable Long id) {
