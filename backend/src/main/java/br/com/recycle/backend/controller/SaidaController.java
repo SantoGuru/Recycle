38f97ec675fb7dc44bc19cf93b3928d20bffd6b1
SantoGuru <breno.guiri@gmail.com>
2025-05-29 17:25:11 -0300

    feat: Implementação do AuthenticationPrincipal, melhoria em anotações para o swagger e implementação dos retornos para casos especificos

--- backend/src/main/java/br/com/recycle/backend/controller/SaidaController.java
+++ backend/src/main/java/br/com/recycle/backend/controller/SaidaController.java
@@ -6,12 +6,16 @@ import br.com.recycle.backend.dto.SaidaRequestDTO;
 import br.com.recycle.backend.dto.SaidaResponseDTO;
 import br.com.recycle.backend.service.SaidaService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name= "Saídas", description = "Gerencia o registro e listagem de saídas de materiais do estoque")
@@ -32,9 +36,12 @@ public class SaidaController {
 
  @PostMapping
     public ResponseEntity<List<SaidaResponseDTO>> registrarSaida(
-            @RequestBody List<SaidaRequestDTO> saidas,
-            HttpServletRequest request) {
-        Long usuarioId = (Long) request.getAttribute("usuarioId");
+            @Parameter(description = "Lista das saídas a serem registradas", required = true)
+            @Valid @RequestBody List<SaidaRequestDTO> saidas,
+            @Parameter(hidden = true) @AuthenticationPrincipal Long usuarioId){
+        if (usuarioId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         List<SaidaResponseDTO> resultados = saidaService.registrarSaidas(saidas, usuarioId);
         return ResponseEntity.status(HttpStatus.CREATED).body(resultados);
     }
@@ -45,9 +52,15 @@ public class SaidaController {
     )
 
     @GetMapping
-    public ResponseEntity<List<SaidaResponseDTO>> listarSaidas(HttpServletRequest request) {
-        Long usuarioId = (Long) request.getAttribute("usuarioId");
+    public ResponseEntity<List<SaidaResponseDTO>> listarSaidas(
+        @Parameter(hidden = true) @AuthenticationPrincipal Long usuarioId){
+        if (usuarioId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         List<SaidaResponseDTO> saidas = saidaService.listarSaidas(usuarioId);
+        if (saidas.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(saidas);
     }
 }
