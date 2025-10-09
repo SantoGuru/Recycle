e62b085c6f83c23ef16f4af64b63aaea269d32ce
SantoGuru <breno.guiri@gmail.com>
2025-10-09 15:35:53 -0300

    feat: Implementa DTO provisorio de funcionario

---
+++ backend/src/main/java/br/com/recycle/backend/dto/FuncionarioDTO.java
@@ -0,0 +1,71 @@
+package br.com.recycle.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class FuncionarioDTO {
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 2 e 50 caracteres.")
+    private String nome;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
+    @Size(max = 255, message = "O email não pode exceder 255 caracteres.")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, max = 50, message = "A senha deve ter entre 6 e 50 caracteres.")
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$",
+        message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula e um dígito."
+    )
+    private String senha;
+
+    private String nomeFantasia;
+
+    private String cnpj;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
