746bbdd5b264230a21c2fe2e7e1a4c5e921dc2f2
Walther Oliveira <waltherpires@hotmail.com>
2025-10-10 17:38:53 -0300

    feat: integra tela de funcionarios com backend

--- mobile/app/(tabs)/(home)/funcionarios.tsx
+++ mobile/app/(tabs)/(home)/funcionarios.tsx
@@ -1,12 +1,35 @@
 import IconCard from "@/components/ui/IconCard";
+import { API_URL } from "@/config";
+import { useAuth } from "@/context/AuthContext";
 import { router } from "expo-router";
 import { useState, useEffect, useMemo } from "react";
-import { View, StyleSheet } from "react-native";
-import { DataTable, useTheme } from "react-native-paper";
+import { View, StyleSheet, Dimensions } from "react-native";
+import {
+  Avatar,
+  DataTable,
+  MD3Theme,
+  Surface,
+  Text,
+  useTheme,
+} from "react-native-paper";
+const { width, height } = Dimensions.get("window");
+
+interface Funcionario {
+  id: number;
+  nome: string;
+  entradas: number;
+  saidas: number;
+}
 
 export default function Funcionarios() {
+  const { session } = useAuth();
+  const token = session?.token;
+  const empresaNome = session?.empresaNome;
+
   const theme = useTheme();
+  const style = useMemo(() => styles(theme), [theme]);
   const [page, setPage] = useState<number>(0);
+  const [items, setItems] = useState<Funcionario[]>([]);
   const numberOfItemsPerPageList = useMemo(() => [1, 2, 5, 10], []);
   const [itemsPerPage, onItemsPerPageChange] = useState(
     numberOfItemsPerPageList[0]
@@ -22,78 +45,69 @@ export default function Funcionarios() {
     }
   };
 
-  const [items] = useState([
-    {
-      key: 1,
-      nome: "Daniel Mascarenhas",
-      entradas: 3,
-      saidas: 1,
-    },
-    {
-      key: 2,
-      nome: "Breno",
-      entradas: 3,
-      saidas: 1,
-    },
-    {
-      key: 3,
-      nome: "Walther",
-      entradas: 3,
-      saidas: 1,
-    },
-  ]);
+  useEffect(() => {
+    // fix: o backend no momento nao retorna entradas e saidas
+    const fetchFuncionarios = async () => {
+      try {
+        const response = await fetch(`${API_URL}/api/usuarios/funcionarios`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+          setItems(data);
+        }
+      } catch (e) {
+        return { error: "Não foi possível conectar ao servidor" };
+      }
+    };
+    fetchFuncionarios();
+    setPage(0);
+  }, [itemsPerPage, token]);
 
   const from = page * itemsPerPage;
   const to = Math.min((page + 1) * itemsPerPage, items.length);
 
-  useEffect(() => {
-    setPage(0);
-  }, [itemsPerPage]);
-
-  const styles = StyleSheet.create({
-    body: {
-      flex: 1,
-      padding: 8,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: theme.colors.background,
-    },
-    grid: {
-      width: "100%",
-      flexDirection: "row",
-      flexWrap: "wrap",
-      justifyContent: "center",
-      alignContent: "space-between",
-      gap: 8,
-    },
-    table: {
-      backgroundColor: theme.colors.surface,
-      borderRadius: theme.roundness,
-      elevation: 4,
-      shadowColor: theme.colors.shadow,
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-    },
-  });
-
   return (
-    <View style={styles.body}>
-      <View style={styles.grid}>
-        <IconCard
-          iconName="person-add"
-          title="Cadastrar Funcionário"
-          onPress={() => router.push("/(tabs)/(home)/cadastroFuncionario")}
-        />
+    <View style={style.body}>
+      <View style={{ width: "100%", gap: height * 0.03 }}>
+        <View style={style.header}>
+          <Avatar.Image
+            size={48}
+            source={require("@/assets/images/recycle-logo.png")}
+            style={{
+              backgroundColor: theme.colors.surface,
+            }}
+          />
+          <View>
+            <Text
+              variant="headlineSmall"
+              style={{ color: theme.colors.onSurfaceVariant }}
+            >
+              {empresaNome}
+            </Text>
+          </View>
+        </View>
+        <View style={style.grid}>
+          <IconCard
+            iconName="person-add"
+            title="Cadastrar Funcionário"
+            onPress={() => router.push("/(tabs)/(home)/cadastroFuncionario")}
+          />
+        </View>
       </View>
-      <DataTable style={styles.table}>
+      <DataTable style={style.table}>
         <DataTable.Header>
           <DataTable.Title>Nome</DataTable.Title>
           <DataTable.Title numeric>Entradas</DataTable.Title>
           <DataTable.Title numeric>Saídas</DataTable.Title>
         </DataTable.Header>
         {items.slice(from, to).map((item) => (
-          <DataTable.Row key={item.key}>
+          <DataTable.Row key={item.id}>
             <DataTable.Cell>{item.nome}</DataTable.Cell>
             <DataTable.Cell numeric>{item.entradas}</DataTable.Cell>
             <DataTable.Cell numeric>{item.saidas}</DataTable.Cell>
@@ -116,3 +130,45 @@ export default function Funcionarios() {
     </View>
   );
 }
+
+const styles = (theme: MD3Theme) =>
+  StyleSheet.create({
+    body: {
+      flex: 1,
+      padding: 8,
+      gap: height * 0.06,
+      justifyContent: "flex-start",
+      alignItems: "center",
+      backgroundColor: theme.colors.background,
+    },
+    header: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      gap: width * 0.08,
+      width: "100%",
+      padding: 16,
+      borderRadius: 12,
+      alignItems: "center",
+      borderBottomColor: theme.colors.outline,
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      backgroundColor: theme.colors.surface,
+    },
+    grid: {
+      width: "100%",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      alignContent: "space-between",
+      gap: 8,
+    },
+    table: {
+      backgroundColor: theme.colors.surface,
+      borderRadius: theme.roundness,
+      elevation: 4,
+      shadowColor: theme.colors.shadow,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 3,
+    },
+  });
