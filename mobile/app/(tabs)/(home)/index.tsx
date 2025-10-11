0db21d9922e45172e9868899d71931c9c36bfd72
Walther Oliveira <waltherpires@hotmail.com>
2025-10-11 02:54:21 -0300

    feat: adiciona integracao para resumo de dashboard

--- mobile/app/(tabs)/(home)/index.tsx
+++ mobile/app/(tabs)/(home)/index.tsx
@@ -5,15 +5,54 @@ import IconCard from "@/components/ui/IconCard";
 import { useAuth } from "@/context/AuthContext";
 import { router } from "expo-router";
 import { Avatar, Surface, Text, useTheme } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { API_URL } from "@/config";
 
 const { width } = Dimensions.get("window");
+
+interface Dashboard {
+  materiaisComEstoqueBaixo: number;
+  quantidadeTotalKg: number;
+  totalMateriais: number;
+  valorTotalEstoque: number;
+}
 
 export default function HomeScreen() {
   const theme = useTheme();
   const { session } = useAuth();
   const role = session?.role;
+  const token = session?.token;
   const nome = session?.nome.split(" ")[0];
   const empresaNome = session?.empresaNome;
+  const [dashboard, setDashboard] = useState<Dashboard>({
+    materiaisComEstoqueBaixo: 0,
+    quantidadeTotalKg: 0,
+    totalMateriais: 0,
+    valorTotalEstoque: 0,
+  });
+
+  useEffect(() => {
+    const fetchDashboard = async () => {
+      try {
+        const response = await fetch(`${API_URL}/api/dashboard/resumo`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+          setDashboard(data);
+        }
+      } catch (e) {
+        return { error: "Não foi possível conectar ao servidor" };
+      }
+    };
+
+    fetchDashboard();
+  }, [token]);
 
   let isAdmin;
   if (role === "GERENTE") {
@@ -47,15 +86,17 @@ export default function HomeScreen() {
       <View style={styles.resume}>
         <Surface elevation={2} style={styles.card}>
           <View style={styles.materialContent}>
-            <Text variant="displayMedium">32</Text>
-            <Text variant="bodySmall" style={{ color: 'rgb(56, 107, 1)' }}>(R$ 20,40)</Text>
+            <Text variant="displayMedium">{dashboard.totalMateriais}</Text>
+            <Text variant="bodySmall" style={{ color: "rgb(56, 107, 1)" }}>
+              R$ {dashboard.valorTotalEstoque}
+            </Text>
           </View>
           <Text variant="labelMedium">Total de Materiais</Text>
         </Surface>
         <Surface elevation={2} style={styles.card}>
           <View style={styles.materialContent}>
             <Text variant="displayMedium" style={{ color: theme.colors.error }}>
-              2
+              {dashboard.materiaisComEstoqueBaixo}
             </Text>
             <Text variant="bodySmall"> </Text>
           </View>
