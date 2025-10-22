89a6add6dc544d288eaf4a381587f3d7f3e05706
AliceJDauphin <105870654+AliceJDauphin@users.noreply.github.com>
2025-10-22 14:35:01 -0300

    Feedback de erros para o usuário
    modificação do registro para mostrar as mensagens de erro de validação

--- frontend/src/http.jsx
+++ frontend/src/http.jsx
@@ -8,28 +8,13 @@ import { BadgeCheck, CircleAlert } from "lucide-react";
  * @returns 
  */
 export async function createAccountFunction(bodyData) {
-  try {
-    const response = await fetch("http://localhost:8080/api/auth/registro", {
+  const response = await fetch("http://localhost:8080/api/auth/registro", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(bodyData),
     });
 
-    if (response.ok) {
-      toast.success("Conta criada!", {
-        icon: <BadgeCheck className="stroke-blue-500" />,
-        className:
-          "border-1 border-blue-600 bg-white text-blue-600 font-bold rounded-sm",
-      });
-    }
-
     return response;
-  } catch (error) {
-    toast.error(`Erro de rede: ${error.message}`, {
-      className: "border-1 border-red-600 bg-white text-red-600 font-bold rounded-sm",
-    });
-    throw error;
-  }
 }
 
 /**
